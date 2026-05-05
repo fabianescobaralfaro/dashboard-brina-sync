5d350f10229890db86049f0107fc2b9a32c61f2b
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-05-04 21:14:41 -0400

    Actualizacion automatica — 04/05/2026 21:14

--- haulmer-data.js
+++ haulmer-data.js
@@ -1,39 +1,93 @@
 /* Generado automáticamente por scraper.py — NO editar manualmente */
 window.HAULMER_DATA = {
-  "actualizadoEl": "04/05/2026 15:15",
+  "actualizadoEl": "04/05/2026 21:12",
   "error": null,
   "informe_dia": {
     "periodo": "Día",
-    "monto_neto": 161033,
-    "iva": 30607,
-    "monto_ventas": 191640,
-    "monto_exento": 191640,
-    "propinas": 6230,
-    "debito": 122320,
-    "credito": 3950,
-    "efectivo": 21900
+    "monto_neto": 265388,
+    "iva": 50432,
+    "monto_ventas": 315820,
+    "monto_exento": 315820,
+    "propinas": 7780,
+    "debito": 217220,
+    "credito": 10300,
+    "efectivo": 96080,
+    "pos_breakdown": [
+      {
+        "serial": "PB1T238420182",
+        "total_ventas": 236020,
+        "debito": 151540,
+        "credito": 10300,
+        "efectivo": 74180,
+        "propinas": 0
+      },
+      {
+        "serial": "6010B232511900275",
+        "total_ventas": 87580,
+        "debito": 65680,
+        "credito": 0,
+        "efectivo": 21900,
+        "propinas": 0
+      }
+    ]
   },
   "informe_semana": {
     "periodo": "Semana",
-    "monto_neto": 161033,
-    "iva": 30607,
-    "monto_ventas": 191640,
-    "monto_exento": 191640,
-    "propinas": 6230,
-    "debito": 122320,
-    "credito": 3950,
-    "efectivo": 21900
+    "monto_neto": 265388,
+    "iva": 50432,
+    "monto_ventas": 315820,
+    "monto_exento": 315820,
+    "propinas": 7780,
+    "debito": 217220,
+    "credito": 10300,
+    "efectivo": 96080,
+    "pos_breakdown": [
+      {
+        "serial": "PB1T238420182",
+        "total_ventas": 236020,
+        "debito": 151540,
+        "credito": 10300,
+        "efectivo": 74180,
+        "propinas": 0
+      },
+      {
+        "serial": "6010B232511900275",
+        "total_ventas": 87580,
+        "debito": 65680,
+        "credito": 0,
+        "efectivo": 21900,
+        "propinas": 0
+      }
+    ]
   },
   "informe_mes": {
     "periodo": "Mes",
-    "monto_neto": 161033,
-    "iva": 30607,
-    "monto_ventas": 191640,
-    "monto_exento": 191640,
-    "propinas": 6230,
-    "debito": 122320,
-    "credito": 3950,
-    "efectivo": 21900
+    "monto_neto": 265388,
+    "iva": 50432,
+    "monto_ventas": 315820,
+    "monto_exento": 315820,
+    "propinas": 7780,
+    "debito": 217220,
+    "credito": 10300,
+    "efectivo": 96080,
+    "pos_breakdown": [
+      {
+        "serial": "PB1T238420182",
+        "total_ventas": 236020,
+        "debito": 151540,
+        "credito": 10300,
+        "efectivo": 74180,
+        "propinas": 0
+      },
+      {
+        "serial": "6010B232511900275",
+        "total_ventas": 87580,
+        "debito": 65680,
+        "credito": 0,
+        "efectivo": 21900,
+        "propinas": 0
+      }
+    ]
   },
   "dte": {
     "total": 0,
