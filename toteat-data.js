0b5b010f5514d31310908e21a30aba2823b32c4b
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-05-06 22:39:01 -0400

    Actualizacion automatica — 06/05/2026 22:38

--- toteat-data.js
+++ toteat-data.js
@@ -2,21 +2,76 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "05/05/2026",
-    "fecha_iso": "2026-05-05",
-    "turno": "1494747985014084-00001-20260505TB.001",
-    "apertura": "",
+    "fecha": "06/05/2026",
+    "fecha_iso": "2026-05-06",
+    "turno": "1494747985014084-00001-20260506TB.001",
+    "apertura": "2026-05-06T12:57:54",
     "cierre": "",
-    "total_ventas": 0,
-    "total_neto": 0,
-    "total_iva": 0,
-    "n_ordenes": 0,
+    "total_ventas": 44370.0,
+    "total_neto": 49049.0,
+    "total_iva": 9321.0,
+    "n_ordenes": 7,
     "n_anuladas": 0,
-    "efectivo": 0,
-    "tarjeta": 0,
-    "productos_top": []
+    "efectivo": 2200.0,
+    "tarjeta": 43548.0,
+    "productos_top": [
+      {
+        "nombre": "Ciabatta con uova",
+        "cantidad": 2,
+        "total": 11000.0
+      },
+      {
+        "nombre": "Huerto & Formaggi - Vegetariana",
+        "cantidad": 1,
+        "total": 8390.0
+      },
+      {
+        "nombre": "Carni & Speciali - Pepperoni",
+        "cantidad": 1,
+        "total": 8390.0
+      },
+      {
+        "nombre": "KitKat 41,5 grs",
+        "cantidad": 3,
+        "total": 3000.0
+      },
+      {
+        "nombre": "Mocaccino",
+        "cantidad": 1,
+        "total": 2500.0
+      },
+      {
+        "nombre": "Americano",
+        "cantidad": 1,
+        "total": 2500.0
+      },
+      {
+        "nombre": "Lays 45 grs",
+        "cantidad": 2,
+        "total": 2400.0
+      },
+      {
+        "nombre": "Jugo natural 350 cc",
+        "cantidad": 1,
+        "total": 2300.0
+      },
+      {
+        "nombre": "Bebida 350 cc",
+        "cantidad": 1,
+        "total": 1890.0
+      },
+      {
+        "nombre": "Bigtime celeste",
+        "cantidad": 2,
+        "total": 1000.0
+      }
+    ]
   },
   "historial_cierres": [
+    {
+      "fecha": "2026-05-06",
+      "turno": "1494747985014084-00001-20260506TB.001"
+    },
     {
       "fecha": "2026-05-05",
       "turno": "1494747985014084-00001-20260505TB.001"
@@ -60,12 +115,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-04-16",
       "turno": "1494747985014084-00001-20260416TB.001"
-    },
-    {
-      "fecha": "2026-04-15",
-      "turno": "1494747985014084-00001-20260415TB.002"
     }
   ],
-  "total_cierres_historicos": 30,
-  "actualizadoEl": "05/05/2026 20:49"
+  "total_cierres_historicos": 31,
+  "actualizadoEl": "06/05/2026 22:36"
 };
